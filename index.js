0e18d0f831af05bdbc122b935690bdc86ad4448c
puppybits <bobby@getfiskal.com>
2019-09-23 11:20:21 -0700

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var root = 'https://cdn.jsdelivr.net/gh/puppybits/phaser3-game@master/'
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -24,11 +25,11 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.image('sky', root+'assets/sky.png');
+    this.load.image('ground', root+'assets/platform.png');
+    this.load.image('star', root+'assets/star.png');
+    this.load.image('bomb', root+'assets/bomb.png');
+    this.load.spritesheet('dude', root+'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create ()
