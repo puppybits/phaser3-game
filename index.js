708104556f940b7d55608e0650e7d9e2585c57d3
puppybits <bobby@getfiskal.com>
2019-09-29 07:21:55 -0700

    1 file modified

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ var config = {
     }
 };
 
-var player;
 var platforms;
-var cursors;
-
 var game = new Phaser.Game(config);
 
 function preload ()
@@ -29,74 +26,30 @@ function preload ()
     this.load.image('ground', root+'assets/platform.png');
     this.load.image('star', root+'assets/star.png');
     this.load.image('bomb', root+'assets/bomb.png');
-    this.load.spritesheet('dude', root+'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+
+    // add preload code below this line
+
+
+    
 }
 
 function create ()
 {
     this.add.image(400, 300, 'sky');
-
     platforms = this.physics.add.staticGroup();
-
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
 
-    player = this.physics.add.sprite(100, 450, 'dude');
+    // add create code below this line
 
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
 
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
 
-    this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
-        frameRate: 20
-    });
 
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    cursors = this.input.keyboard.createCursorKeys();
-
-    this.physics.add.collider(player, platforms);
 }
 
 function update ()
 {
-    if (cursors.left.isDown)
-    {
-        player.setVelocityX(-160);
-
-        player.anims.play('left', true);
-    }
-    else if (cursors.right.isDown)
-    {
-        player.setVelocityX(160);
-
-        player.anims.play('right', true);
-    }
-    else
-    {
-        player.setVelocityX(0);
-
-        player.anims.play('turn');
-    }
-
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.setVelocityY(-330);
-    }
+    
 }
