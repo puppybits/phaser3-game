15d55ffb42fc44583765d4c92721228e79488281
puppybits <bobby@getfiskal.com>
2019-09-29 07:27:18 -0700

    1 file modified

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ var config = {
 var platforms;
 var game = new Phaser.Game(config);
 
+var player;
+var cursors;
+ 
 function preload ()
 {
     this.load.image('sky', root+'assets/sky.png');
@@ -29,7 +32,7 @@ function preload ()
 
     // add preload code below this line
 
-
+    this.load.spritesheet('dude', root+'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
     
 }
 
@@ -44,12 +47,58 @@ function create ()
 
     // add create code below this line
 
+    player = this.physics.add.sprite(100, 450, 'dude');
+    this.physics.add.collider(player, platforms);
 
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update ()
 {
-    
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
 }
